a0a7a791d193ef9c0feb9954bab90e6c3bf024fe
Sinan Eldem <sinan@sinaneldem.com.tr>
2014-03-12 13:49:11 +0200

    Validation güncellemeleri

--- tr/validation.php
+++ tr/validation.php
@@ -67,9 +67,9 @@ return array(
     "required"       => ":attribute alanı gereklidir.",
     "required_if"      => ":attribute alanı, :other :value değerine sahip olduğunda zorunludur.",
     "required_with"   => ":attribute alanı :values varken zorunludur.",
-    "required_with_all" => "The :attribute field is required when :values is present.",
+    "required_with_all" => ":attribute alanı :values varken zorunludur.",
     "required_without" => ":attribute alanı :values yokken zorunludur.",
-    "required_without_all" => "The :attribute field is required when none of :values are present.",
+    "required_without_all" => ":attribute alanı :values yokken zorunludur.",
     "same"           => ":attribute ile :other eşleşmelidir.",
     "size"           => array(
         "numeric" => ":attribute :size olmalıdır.",
